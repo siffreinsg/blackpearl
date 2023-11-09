82d9378ae5551578cbd2bc89d2d67a6c75338e19
Siffrein Sigy <hello@siffreinsigy.me>
2023-11-09 17:01:15 +0100

    Moved radarr class to independent module for reusability

--- python/notify_missing.py
+++ python/notify_missing.py
@@ -5,6 +5,7 @@ import dotenv
 import requests
 import timeago
 import yaml
+from radarr import Radarr
 
 # Load environment variables
 dotenv.load_dotenv()
@@ -54,68 +55,6 @@ def load_config():
         raise Exception(f"Error while parsing config.yaml: {e}")
     except Exception as e:
         raise Exception(f"Error while loading config.yaml: {e}")
-
-
-# ==== RADARR ====
-class Radarr:
-    def __init__(self, url, api_key):
-        if not url or not api_key:
-            raise Exception('Missing Radarr URL or API key')
-
-        self.url = url
-        self.api_key = api_key
-
-    def get_system_status(self):
-        response = requests.get(f'{self.url}/api/v3/system/status?apikey={self.api_key}')
-        response.raise_for_status()
-        system_status = response.json()
-
-        return system_status
-
-    def get_movies(self):
-        response = requests.get(f'{self.url}/api/v3/movie?apikey={self.api_key}')
-        response.raise_for_status()
-        movies = response.json()
-
-        return movies
-
-    def get_tags(self):
-        response = requests.get(f'{self.url}/api/v3/tag?apikey={self.api_key}')
-        response.raise_for_status()
-        tags = response.json()
-
-        return tags
-
-    def add_movie_tag(self, movie_ids, tags):
-        payload = {
-            'movieIds': movie_ids if isinstance(movie_ids, list) else [movie_ids],
-            'applyTags': 'add',
-            'tags': tags if isinstance(tags, list) else [tags]
-        }
-
-        response = requests.put(f'{self.url}/api/v3/movie/editor/?apikey={self.api_key}', json=payload)
-        response.raise_for_status()
-
-    def remove_movie_tag(self, movie_ids, tags):
-        payload = {
-            'movieIds': movie_ids if isinstance(movie_ids, list) else [movie_ids],
-            'applyTags': 'remove',
-            'tags': tags if isinstance(tags, list) else [tags]
-        }
-
-        response = requests.put(f'{self.url}/api/v3/movie/editor/?apikey={self.api_key}', json=payload)
-        response.raise_for_status()
-
-    def create_tag(self, label):
-        payload = {
-            'label': label
-        }
-
-        response = requests.post(f'{self.url}/api/v3/tag/?apikey={self.api_key}', json=payload)
-        response.raise_for_status()
-
-        tag_id = response.json()['id']
-        return tag_id
 
 
 if __name__ == '__main__':
