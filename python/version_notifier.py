228c223c4ac465feb12f28d2df2c075975df1a52
Siffrein Sigy <hello@siffreinsigy.me>
2023-11-09 16:59:33 +0100

    Added url to notification

--- python/version_notifier.py
+++ python/version_notifier.py
@@ -140,7 +140,10 @@ if __name__ == '__main__':
                 continue
 
             version = release.get("tag_name")
+            url = release.get("html_url")
             print(f"Update found for {display_name}: {version}.")
             update_version_file(service, version)
 
-            notify_tautulli(f"Mise à jour de <b>{display_name}</b> à la version <code>{version}</code> disponible.")
+            notify_tautulli(
+                f"Mise à jour de <b>{display_name}</b> à la version <code>{version}</code> disponible.\n{url}"
+            )
