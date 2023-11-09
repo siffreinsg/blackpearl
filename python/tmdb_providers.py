426c10786db5c9fa7152124b03d3e9c310783fb8
Siffrein Sigy <hello@siffreinsigy.me>
2023-11-09 17:00:38 +0100

    Reworked script

--- python/tmdb_providers.py
+++ python/tmdb_providers.py
@@ -1,3 +1,15 @@
+'''
+Description:  Generates a list of regions and providers for TMDb
+Author:       /u/siffreinsg
+Requires:     dotenv, requests
+
+Environment variables:
+    * TMDB_API_KEY - TMDb API key
+    * ST_APP_DIR - Path to the Streaming Tagger app directory
+
+Usage:
+    python tmdb_providers.py
+'''
 import os
 
 import dotenv
@@ -5,23 +17,24 @@ import requests
 
 dotenv.load_dotenv()
 TMDB_API_KEY = os.getenv('TMDB_API_KEY')
-ST_APP_DIR = os.getenv('ST_APP_DIR', os.path.join(os.path.expanduser('~'), '.apps/streaming-tag'))
+ST_APP_DIR = os.getenv('ST_APP_DIR', os.path.join(os.path.expanduser('~'), '.apps/streaming-taggerr'))
 
-tmdbHeaders = {'Content-Type': 'application/json'}
+headers = {'Content-Type': 'application/json'}
 
-tmdbResponseRegions = requests.get(f'https://api.themoviedb.org/3/watch/providers/regions?api_key={TMDB_API_KEY}', headers=tmdbHeaders)
-tmdbRegions = tmdbResponseRegions.json()
+responseRegions = requests.get(f'https://api.themoviedb.org/3/watch/providers/regions?api_key={TMDB_API_KEY}', headers=headers)
+regions = responseRegions.json()["results"]
 
-tmdbResponseProviders = requests.get(f'https://api.themoviedb.org/3/watch/providers/movie?api_key={TMDB_API_KEY}', headers=tmdbHeaders)
-tmdbProviders = tmdbResponseProviders.json()
+responseProviders = requests.get(f'https://api.themoviedb.org/3/watch/providers/movie?api_key={TMDB_API_KEY}', headers=headers)
+providers = responseProviders.json()
 
-allProviders = [p["provider_name"] for p in tmdbProviders["results"]]
-providers = sorted(set(allProviders))
+allProviders = [p["provider_name"] for p in providers["results"]]
+providerNames = sorted(set(allProviders))
 
 with open(os.path.join(ST_APP_DIR, "providers.txt"), "w") as f:
     f.write("Regions\n-------\n")
-    for r in tmdbRegions["results"]:
-        f.write(str(r["iso_3166_1"])+"\t"+str(r["english_name"])+"\n")
+    for region in regions:
+        f.write(f"{region['iso_3166_1']}\t{region['english_name']}\n")
+
     f.write("\n\nProviders\n---------\n")
-    for p in providers:
-        f.write(str(p)+"\n")
+    for provider in providerNames:
+        f.write(f"{provider}\n")
